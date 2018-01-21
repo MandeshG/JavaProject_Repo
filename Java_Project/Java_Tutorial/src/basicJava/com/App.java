2f477e88fcd976180f4c97a607fe074c873f8c46
MandeshG <mandeshgiri7412@gmail.com>
2018-01-21 14:54:04 -0500

    Made a Change in App Java

--- Java_Project/Java_Tutorial/src/basicJava/com/App.java
+++ Java_Project/Java_Tutorial/src/basicJava/com/App.java
@@ -3,7 +3,7 @@ package basicJava.com;
 public class App {
 	public static void main(String[] args) {
 
-		int value[] = new int[7];
+		int value[] = new int[8];
 		value[0] = 10;
 		value[1] = 15;
 		value[2] = 20;
@@ -11,13 +11,14 @@ public class App {
 		value[4] = 65;
 		value[5] = 70;
 		value[6] = 80;
+		value[7]=90;
 		
 		for (int i = 0; i < value.length-1; i++) {
 			
 		}
 
 		
-		System.out.println(value[5]);
+		System.out.println(value[6]);
 
 	}
 
